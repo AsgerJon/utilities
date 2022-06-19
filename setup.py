32fa0052e7bbc14205ebfb684580ddb969c7ae27
AsgerJon <asgerjon2@gmail.com>
2022-06-19 17:33:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
-"""vistutils is a collection of modules providing helpful utility
-functions"""
+import setuptools
 
-from setuptools import setup, find_packages
-import pathlib
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-here = pathlib.Path(__file__).parent.resolve()
-long_description = (here / "README.md").read_text(encoding="utf-8")
-
-setup(
-  name="Vistutils", version="0.0.1",
-  description="""vistutils is a collection of modules providing helpful 
-  utility functions""", long_description=long_description,
-  long_description_content_type="text/markdown",
-  url="https://github.com/AsgerJon/vistutils",  # Optional
-  author="Asger Jon Vistisen",
-  author_email="asgerjon2@gmail.com",
-  packages=find_packages(include=["waila"])
+setuptools.setup(
+    name="vistutils",
+    version="0.0.1",
+    author="Asger Jon Vistisen",
+    description="Helpful utility functions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+    py_modules=["vistutils"],
+    package_dir={'':'vistutils/src'},
+    install_requires=[]
 )
